a2a8aa292c926d127d47055e63ad10fa50294950
Alvaro Basso <65720908+AlvaroBassoGit@users.noreply.github.com>
2021-11-05 21:06:14 -0300

    Update 18.py

--- 18.py
+++ 18.py
@@ -5,15 +5,10 @@ produto1 = float(input ("Produto 1, R$: "))
 produto2 = float(input ("Produto 2, R$: "))
 produto3 = float(input ("Produto 3, R$: "))
 
-if produto1 < produto2:
-    print ("compre o produto 1 pois é mais barato")
-elif produto1 < produto3:
-    print ("compre o produto 1 pois é mais barato")
-    
-elif produto2 < produto3:
-    print ("compre o produto 2 pois é mais barato")
-elif produto2 < produto1:
-    print ("compre o produto 2 pois é mais barato")
- 
-else:
-    print ("compre o produto 3 pois é mais barato")
+print("resposta")
+if (produto1 <= produto2) and (produto1 <= produto3):
+    print ("compre produto 1")
+elif (produto2 <= produto1) and (produto2 <= produto3):
+    print ("compre produto 2")
+elif (produto3 <= produto1) and (produto3 <= produto2):
+    print ("compre produto 3")
