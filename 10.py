74783031b6785f3e3ae41be5ea33112dd42adfa4
Alvaro Basso <65720908+AlvaroBassoGit@users.noreply.github.com>
2021-11-07 21:59:18 -0300

    Delete 10.py

--- 10.py
+++
@@ -1,9 +0,0 @@
-nota_1 = input ("Escreva a nota: ")
-nota_1 = float (nota_1)
-
-nota_2 = input ("Escreva a nota: ")
-nota_2 = float (nota_2)
-
-resultado = (nota_1 + nota_2) / 2 
-
-print (resultado)
