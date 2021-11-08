4b9c1dc94fe10ee19df1f798da9adf2d1f2e9c2b
Alvaro Basso <65720908+AlvaroBassoGit@users.noreply.github.com>
2021-11-07 21:59:29 -0300

    Delete 12.py

--- 12.py
+++
@@ -1,9 +0,0 @@
-numero_usuario = float(input("Escreva o número: "))
-if numero_usuario > 0:
-    print ("Positivo")
-if numero_usuario < 0:
-    print ("Negativo")
-if numero_usuario == 0:
-    print ("Neutro")
-
-    
